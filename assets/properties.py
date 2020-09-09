9a9d3fb2b92e27e94c28f1bb5fb2f0103ce3ae95
TRSTN4 <TRSTN4@users.noreply.github.com>
2020-09-09 16:45:09 +0200

    Version Update

--- assets/properties.py
+++ assets/properties.py
@@ -3,7 +3,7 @@
 total_major_changes = 1
 total_modules = 1
 total_categories = 1
-total_scripts = 1
+total_scripts = 0
 total_content = total_scripts
 version = str(total_major_changes) + '.' + str(total_modules) + '.' + str(total_categories) + '.' + str(total_content)
 
