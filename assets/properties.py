963d3ab811ece3d638e678444679444807cc26ef
TRSTN4 <TRSTN4@users.noreply.github.com>
2020-09-09 16:35:49 +0200

    Version 1.1.1.0 Update

--- assets/properties.py
+++ assets/properties.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 total_major_changes = 1
-total_modules = 0
-total_categories = 0
+total_modules = 1
+total_categories = 1
 total_scripts = 0
 total_content = total_scripts
 version = str(total_major_changes) + '.' + str(total_modules) + '.' + str(total_categories) + '.' + str(total_content)
